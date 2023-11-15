45025a7b56a80b2ef2b3e96e890571c6b331b868
Rhys Morgan <rhysm@me.com>
2023-11-15 08:17:57 +0000

    Working TCA 1.4 branch

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.3.0"),
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "case-key-paths"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.4.0"),
   ],
   targets: [
     .target(
